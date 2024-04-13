311db8aae0a51ee0b442418b47c751f229907179
Arthur1asdf <arthurteng0819@gmail.com>
2024-04-13 13:48:21 -0400

    setting up

---
+++ src/Railroad.java
@@ -0,0 +1,14 @@
+public class Railroad {
+    //numOfTracks is the number of lines in each text file
+    //allPossible tracks take in the txt file for the tracks
+    public Railroad(int numOfTracks, String allPossibleTracks )
+    {
+
+    }
+    public String buildRailroad()
+    {
+        //System.out.println(src + "---" + dest + "\t$" + resultingtracks[i].weight + "\n");
+        return ".";
+    }
+}
+
